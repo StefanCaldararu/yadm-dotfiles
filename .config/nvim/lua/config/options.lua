eea55cd0f487c956bd4797aee7ebf652c4f790d7
Stefan Caldararu <stefancaldararu@gmail.com>
2026-09-22 21:17:16 -0600

    :construction:

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+opt.number = false -- No line numbers
+opt.relativenumber = false
+opt.list = false -- Don't draw characters for tabs and trailing spaces
+opt.wrap = true -- Wrap long lines (at word boundaries, via LazyVim's linebreak)
---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,10 @@
+return {
+	-- No indent guides or current-scope highlighting
+	{
+		"folke/snacks.nvim",
+		opts = {
+			indent = { enabled = false },
+			scope = { enabled = false },
+		},
+	},
+}
